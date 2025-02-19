513d071e2ce0f7a5d811b32055b747bb4bbeda04
= <=>
2025-02-19 16:18:19 +0100

    modif_name_repo

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.abspath("./data-processing"))
 sys.path.append(os.path.abspath("./visualisation"))
 sys.path.append(os.path.abspath("./experiments"))
 sys.path.append(os.path.abspath("./deployment"))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
 from model_serving.script.evaluate import evaluate_model, load_preprocessed_model
 from model_serving.script.generate import generate_and_save_model
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project_root",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Ajoute tes dépendances ici (ex: torch, flask...)
+    ],
+)
